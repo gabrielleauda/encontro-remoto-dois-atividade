2bdec8d43eda0f49e69cb1db2e4389dcb76ceb6a
gabrielleauda <gabi_auda@hotmail.com>
2022-07-05 18:45:16 -0300

    encontro remoto

---
+++ segundo-encontro-atividade.js
@@ -0,0 +1,9 @@
+let numeroDeInscricoes = 100
+
+for (let contador = 1; contador < numeroDeInscricoes; contador ++) {
+    console.log(contador);
+
+    if (contador == 67 ) {
+    console.log ("Parabéns, você é o número 67!")
+    };
+}
